6c09ea95629262e7fb928f16af4b21b84a9d3e0a
dima tikhtey <uu36548@gmail.com>
2024-02-20 08:46:53 +0200

    grid item

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+let item = document.querySelector(".client-bottom-text h5");
+item.addEventListener("mouseover", function(e){
+    this.style.cssText = `
+    font-size: 35px;
+    
+    
+    
+    `
+   
+    
+
+});
